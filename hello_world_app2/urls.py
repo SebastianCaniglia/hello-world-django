42a9ede7fabdfd81c6441d8e1e25620113ad570a
SebastianCaniglia <sebastiancaniglia23@gmail.com>
2026-02-24 14:28:02 -0600

    Update urls.py

--- hello_world_app2/urls.py
+++ hello_world_app2/urls.py
@@ -1,8 +1,7 @@
-# hello_world_app2/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"), # Home page
-    path("about/", views.about, name="about"), # About page
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
 ]
